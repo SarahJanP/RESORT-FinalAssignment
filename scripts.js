aae97c6456a1faba0c18743a9c3253d5a4913a47
Sarah Parker <sarah.parker@edu.sait.ca>
2025-04-13 00:27:50 -0600

    Adding some general content

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+
+//WEATHER API
 fetch("https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=a33479c7be343e47718fa8a8544e3d7b&units=metric")
 .then(response => response.json())
 .then(data => {
@@ -7,3 +9,7 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=a33479c7be
     temperature.textContent = "Temperature: " + data.main.temp + "\u00B0C";    
 
 });
+
+
+
+
